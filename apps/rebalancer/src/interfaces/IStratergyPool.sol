c84726cd284bce821f9d85a238062e896d4c1d94
Aadee <aadeetyapatil@gmail.com>
2025-03-11 17:34:28 -0400

    rebalancer

---
+++ apps/rebalancer/src/interfaces/IStratergyPool.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {IPool} from "./IPool.sol";
+
+interface IStratergyPool is IPool {
+    function initialize(
+        address token0_,
+        address controller_,
+        address owner_,
+        address token1_,
+        uint24 poolFee_,
+        address nonfungiblePositionManager_,
+        address swapRouter_,
+        address futuresMarket_,
+        address priceFeed_
+    ) external;
+}
