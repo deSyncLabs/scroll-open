52e749b5e35f1df9ee38f9be69eb3486c000c1c9
Tom Zhang <tommyawesome08@gmail.com>
2025-03-13 10:32:42 -0400

    preliminary open graph image

--- apps/www/src/app/layout.tsx
+++ apps/www/src/app/layout.tsx
@@ -1,38 +1,54 @@
 import type { Metadata } from "next";
-import {  Geist_Mono } from "next/font/google";
+import { Geist_Mono } from "next/font/google";
 import { Navbar } from "@/components/navbar";
 import { Providers } from "@/providers";
 import { cn } from "@/lib/utils";
 import "./globals.css";
 
 const geistMono = Geist_Mono({
-    variable: "--font-geist-mono",
-    subsets: ["latin"],
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-    title: "deSync - Democratizing loans and yield for all",
-    description:
+  title: "deSync - Democratizing loans and yield for all",
+  description:
+    "Using delta neutral stratergies to offer zero-interest loans while providing the best yield to lenders.",
+  keywords: "loans, zero interest, 0 interest, lending",
+  /*openGraph: {
+      title: "deSync - Democratizing loans and yield for all",
+      description:
         "Using delta neutral stratergies to offer zero-interest loans while providing the best yield to lenders.",
+      url: "https://desync.fi/",
+      siteName: "deSync",
+      images: [
+        {
+          url: "./favicon.ico",
+          width: 1200,
+          height: 1200,
+          alt: "deSync",
+        },
+      ],
+      type: "website",
+
+    }*/
 };
 
 export default function RootLayout({
-    children,
+  children,
 }: Readonly<{
-    children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-    return (
-        <html lang="en" suppressHydrationWarning>
-            <body className={cn(geistMono.variable, "antialiased")}>
-                <Providers>
-                    <div className="font-[family-name:var(--font-geist-mono)] flex flex-col items-center w-full min-h-svh">
-                        <Navbar />
-                        <main className="container w-full min-h-svh px-7">
-                            {children}
-                        </main>
-                    </div>
-                </Providers>
-            </body>
-        </html>
-    );
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={cn(geistMono.variable, "antialiased")}>
+        <Providers>
+          <div className="font-[family-name:var(--font-geist-mono)] flex flex-col items-center w-full min-h-svh">
+            <Navbar />
+            <main className="container w-full min-h-svh px-7">{children}</main>
+          </div>
+        </Providers>
+      </body>
+    </html>
+  );
 }
